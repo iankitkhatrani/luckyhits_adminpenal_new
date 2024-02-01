5288c0435a360ac5ce3dcda56778e3b8e30b9aa7
ankit <iankitkhatrani@gmail.com>
2024-02-01 15:28:38 +0530

    add 51.21.131.138

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
     console.log("URL ",url)
 
     if(url[1] != "/signin"){
-      window.location.href =   'http://51.20.117.194:5173/signin'; //"http://192.168.0.203:5173/signin" //
+      window.location.href =   'http://51.21.131.138:5173/signin'; //"http://192.168.0.203:5173/signin" //
       
       return false
     }
